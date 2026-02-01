a2dd7c2aaf89f6bfd0f36c1323feaa6eda33a6b9
teapo1de <1916647616@qq.com>
2026-02-01 19:08:53 +0800

    chore: rename project to EmoGdream

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/emoji-library/',
+  base: '/EmoGdream/',
   plugins: [
     vue(),
     vueDevTools(),
